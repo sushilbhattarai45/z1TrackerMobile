efc2d44ebad6383410ba72a2bde494691e024fdc
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2024-06-14 09:36:43 +0545

    Ui Building

--- app/screens/auth/login.tsx
+++ app/screens/auth/login.tsx
@@ -1,20 +1,582 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useRef, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Alert,
+  Image,
+  Modal,
+  Pressable,
+  Dimensions,
+  KeyboardAvoidingView,
+} from "react-native";
+// import LottieView from "lottie-react-native";
+import colors from "../../components/colors";
+import { sendSms } from "../../components/hitApi";
+import Toast from "react-native-toast-message";
+import { router } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Login = () => {
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [number, setNumber] = React.useState<string>("");
+  const [otp, setOtp] = React.useState({
+    first: "",
+    second: "",
+    third: "",
+    fourth: "",
+  });
+  const [sentOtp, setSentOtp] = React.useState<string | null>(null);
+  useEffect(() => {
+    console.log("OTP", otp.first + otp.second + otp.third + otp.fourth);
+  }, [otp]);
+
+  async function validateNumber() {
+    console.log("Number", number);
+    if (number.length === 10) {
+      Toast.show({
+        type: "success",
+        position: "top",
+        text1: "OTP Sent Successfully",
+        visibilityTime: 2000,
+        autoHide: true,
+        topOffset: 30,
+        bottomOffset: 40,
+      });
+
+      let { success, sendOtp } = await sendSms({ phone: number });
+      console.log("OTP", sendOtp);
+      if (success) {
+        sendOtp.toString();
+        setSentOtp(sendOtp);
+        setModalVisible(true);
+      } else {
+        Toast.show({
+          type: "error",
+          position: "top",
+          text1: "OTP Not Sent! Incorrect Phone Number",
+          visibilityTime: 2000,
+          autoHide: true,
+          topOffset: 30,
+          bottomOffset: 40,
+        });
+      }
+      console.log("OTP", otp);
+    } else {
+      Toast.show({
+        type: "error",
+        position: "top",
+        text1: "Please Enter a valid Number",
+        visibilityTime: 2000,
+        autoHide: true,
+        topOffset: 30,
+        bottomOffset: 40,
+      });
+    }
+  }
+  async function validateOtp() {
+    console.log("OTP", otp.first + otp.second + otp.third + otp.fourth);
+    let otpValue = otp.first + otp.second + otp.third + otp.fourth;
+    if (otpValue == sentOtp) {
+      Toast.show({
+        type: "success",
+        position: "top",
+        text1: "OTP Verified Successfully",
+        visibilityTime: 2000,
+        autoHide: true,
+        topOffset: 30,
+        bottomOffset: 40,
+      });
+      setModalVisible(false);
+      await AsyncStorage.setItem("number", number);
+      router.push("/screens/trackScreen");
+      // await AsyncStorage.setItem("number", number);
+    } else {
+      Toast.show({
+        type: "error",
+        position: "top",
+        text1: "OTP Verification Failed",
+        visibilityTime: 2000,
+        autoHide: true,
+        topOffset: 30,
+        bottomOffset: 40,
+      });
+    }
+  }
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Hello</Text>
+      <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
+        <View
+          style={{
+            backgroundColor: colors.red,
+            flex: 1,
+
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <View
+            style={{
+              justifyContent: "center",
+              flex: 0.6,
+            }}
+          >
+            {/* <LottieView
+            source={require("../assets/lottie/bikemoving.json")}
+            autoPlay
+            loop
+            style={{
+              width: 300,
+              height: 300,
+              alignSelf: "center",
+            }}
+          /> */}
+          </View>
+
+          <View
+            style={{
+              opacity: modalVisible ? 0.8 : 1,
+              flex: 0.4,
+              padding: 20,
+              backgroundColor: colors.white,
+              borderTopLeftRadius: 30,
+              borderTopRightRadius: 30,
+            }}
+          >
+            <View
+              style={{
+                marginTop: 30,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: colors.darkGrey,
+                  fontWeight: "900",
+                }}
+              >
+                Let the Tracking Journey {"\n"}Begin!!
+              </Text>
+            </View>
+            <View
+              style={{
+                marginTop: 20,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: colors.grey,
+                  fontWeight: "600",
+                }}
+              >
+                Enter your Phone Number{" "}
+              </Text>
+            </View>
+            <View
+              style={
+                {
+                  //   marginTop: 10,
+                }
+              }
+            >
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: "gray",
+                  fontWeight: "500",
+                }}
+              >
+                We will send you a One Time Password to the number!
+              </Text>
+            </View>
+            <View
+              style={{
+                marginTop: 10,
+                display: "flex",
+                justifyContent: "space-between",
+                flexDirection: "row",
+              }}
+            >
+              <View
+                style={{
+                  width: 55,
+                  height: 55,
+                  borderRadius: 5,
+                  borderColor: "gray",
+                  borderWidth: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+
+                  padding: 10,
+                }}
+              >
+                <Image
+                  source={require("../../../assets/images/flag.png")}
+                  style={{ width: 30, height: 30 }}
+                />
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                }}
+              >
+                <TextInput
+                  keyboardType="numeric"
+                  maxLength={10}
+                  onChangeText={(value) => {
+                    setNumber(value);
+                  }}
+                  style={{
+                    marginLeft: 5,
+                    height: 55,
+                    borderColor: "gray",
+                    borderWidth: 1,
+                    padding: 10,
+                    paddingLeft: 20,
+                    borderRadius: 10,
+                  }}
+                  placeholder="Enter your Phone Number"
+                />
+              </View>
+            </View>
+            <View
+              style={{
+                marginTop: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: colors.grey,
+                }}
+              >
+                By continuing, you are indicating that you agree to the
+                <Text
+                  style={{
+                    color: colors.driveGreen,
+                    fontWeight: "600",
+                  }}
+                >
+                  {" "}
+                  Terms and Conditions and Privacy Policy
+                </Text>
+              </Text>
+            </View>
+            <View
+              style={{
+                marginTop: 10,
+              }}
+            >
+              <Pressable
+                onPress={() => {
+                  console.log("Hello");
+                  validateNumber();
+                }}
+                style={{
+                  backgroundColor: "#243142",
+                  padding: 10,
+                  height: 52,
+                  marginTop: 10,
+                  marginHorizontal: 5,
+                  borderRadius: 10,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontSize: 18,
+                  }}
+                >
+                  Continue
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+
+      <View style={styles.centeredView}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <View
+                style={{
+                  flex: 0.3,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 18,
+                    color: colors.black,
+                    fontWeight: "800",
+                  }}
+                >
+                  Enter the OTP
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    marginTop: 10,
+                    color: colors.grey,
+                    fontWeight: "500",
+                    textAlign: "center",
+                  }}
+                >
+                  We have sent an OTP to this phone number +977 {number}
+                </Text>
+              </View>
+              <View
+                style={{
+                  display: "flex",
+                  flex: 0.4,
+                  width: "100%",
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                  alignItems: "center",
+                }}
+              >
+                <View
+                  style={{
+                    flex: 0.2,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <TextInput
+                    value={otp.first}
+                    maxLength={1}
+                    onChangeText={(value) => {
+                      setOtp({
+                        ...otp,
+                        first: value,
+                      });
+                      if (value.length > 0) {
+                        this.secondInput.focus();
+                      }
+                    }}
+                    keyboardType="numeric"
+                    style={{
+                      height: 55,
+                      borderColor: "gray",
+                      borderWidth: 1,
+                      padding: 10,
+                      paddingLeft: 20,
+                      borderRadius: 10,
+                    }}
+                  />
+                </View>
+                <View
+                  style={{
+                    flex: 0.2,
+                    marginLeft: 10,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <TextInput
+                    value={otp.second}
+                    ref={(input) => {
+                      this.secondInput = input;
+                    }}
+                    onChangeText={(value) => {
+                      setOtp({
+                        ...otp,
+                        second: value,
+                      });
+                      if (value.length > 0) {
+                        this.thirdInput.focus();
+                      }
+                    }}
+                    maxLength={1}
+                    keyboardType="numeric"
+                    style={{
+                      height: 55,
+                      borderColor: "gray",
+                      borderWidth: 1,
+                      padding: 10,
+                      paddingLeft: 20,
+                      borderRadius: 10,
+                    }}
+                  />
+                </View>
+                <View
+                  style={{
+                    flex: 0.2,
+                    marginLeft: 10,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <TextInput
+                    value={otp.third}
+                    ref={(input) => {
+                      this.thirdInput = input;
+                    }}
+                    onChangeText={(value) => {
+                      setOtp({
+                        ...otp,
+                        third: value,
+                      });
+
+                      if (value.length > 0) {
+                        this.lastInput.focus();
+                      }
+                    }}
+                    maxLength={1}
+                    keyboardType="numeric"
+                    style={{
+                      height: 55,
+                      borderColor: "gray",
+                      borderWidth: 1,
+                      padding: 10,
+                      paddingLeft: 20,
+                      borderRadius: 10,
+                    }}
+                  />
+                </View>
+                <View
+                  style={{
+                    flex: 0.2,
+                    marginLeft: 10,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <TextInput
+                    value={otp.fourth}
+                    ref={(input) => {
+                      this.lastInput = input;
+                    }}
+                    onChangeText={(value) => {
+                      setOtp({
+                        ...otp,
+                        fourth: value,
+                      });
+                      if (value.length > 0) {
+                        // this.nextInput.focus();
+                      }
+                    }}
+                    maxLength={1}
+                    keyboardType="numeric"
+                    style={{
+                      height: 55,
+                      borderColor: "gray",
+                      borderWidth: 1,
+                      padding: 10,
+                      paddingLeft: 20,
+                      borderRadius: 10,
+                    }}
+                  />
+                </View>
+              </View>
+
+              <View
+                style={{
+                  display: "flex",
+                  flex: 0.3,
+                  width: "100%",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Pressable
+                  onPress={() => {
+                    console.log("Hello");
+                    validateOtp();
+                  }}
+                  style={{
+                    backgroundColor: "#243142",
+                    padding: 10,
+                    width: "90%",
+                    height: 52,
+                    marginTop: 10,
+                    borderRadius: 10,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: "white",
+                      fontSize: 18,
+                    }}
+                  >
+                    Verify
+                  </Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  centeredView: {
+    justifyContent: "center",
+    alignSelf: "center",
+
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 20,
+    marginTop: Dimensions.get("window").height / 5,
+    width: Dimensions.get("window").width - 50,
+    height: 300,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});
 
 export default Login;
--- app/screens/trackScreen.tsx
+++ app/screens/trackScreen.tsx
@@ -1,33 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, Text, View } from "react-native";
+import { Button, Image, Switch, Text, View } from "react-native";
 import * as TaskManager from "expo-task-manager";
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
 const LOCATION_TASK_NAME = "background-location-task";
-
-TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
-  if (error) {
-    console.log(error);
-    return;
+import { router } from "expo-router";
+import colors from "../components/colors";
+TaskManager.defineTask(
+  LOCATION_TASK_NAME,
+  ({ data, error }: { data: any; error: any }) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    if (data) {
+      const { locations } = data;
+      console.log(locations[0]);
+      sendMyLocation({
+        locations: locations[0],
+      });
+      // sendPostRequest(locations[0]);
+    }
   }
-  if (data) {
-    const { locations } = data;
-    console.log(locations[0]);
-    sendMyLocation({
-      locations: locations[0],
-      number: "9846761072",
-    });
+);
 
-    // sendPostRequest(locations[0]);
-  }
-});
-
-const sendMyLocation = async (data: { locations: any; number: string }) => {
+const sendMyLocation = async (data: { locations: any }) => {
   try {
-    let number = await AsyncStorage.getItem("number");
+    let num = await AsyncStorage.getItem("number");
     const resp = await axios.get(
       process.env.EXPO_PUBLIC_URL +
         "?lat=" +
@@ -37,7 +38,7 @@ const sendMyLocation = async (data: { locations: any; number: string }) => {
         "&timestamp=" +
         Date.now() +
         "&id=" +
-        number
+        num
     );
     console.log(resp.status);
   } catch (err) {
@@ -59,12 +60,10 @@ const sendMyLocation = async (data: { locations: any; number: string }) => {
 //   }
 // };
 
-export default function TrackScreen() {
+export default function App() {
   const [status, setStatus] = useState("stopped");
-  const [number, setNumber] = useState<string | null>("");
 
   useEffect(() => {
-    checkLogin();
     checkRegistry();
     requestPermissionsAsync();
 
@@ -73,12 +72,8 @@ export default function TrackScreen() {
     // };
   }, []);
 
-  async function checkLogin() {
-    let number = await AsyncStorage.getItem("number");
-    setNumber(number);
-  }
-
   async function checkRegistry() {
+    console.log(await AsyncStorage.getItem("number"));
     console.log(await TaskManager.isTaskRegisteredAsync(LOCATION_TASK_NAME));
 
     setStatus(
@@ -123,15 +118,101 @@ export default function TrackScreen() {
   return (
     <View
       style={{
+        backgroundColor: colors.white,
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            width: "100%",
+            height: 50,
+            marginTop: 30,
+            borderBottomWidth: 0.4,
+            borderBottomColor: colors.grey,
+
+            alignItems: "center",
+            display: "flex",
+            padding: 15,
+            flexDirection: "row",
+            backgroundColor: colors.white,
+          }}
+        >
+          <View
+            style={{
+              flex: 0.3,
+              // backgroundColor: colors.red,
+              justifyContent: "center",
+              alignItems: "flex-start",
+            }}
+          >
+            <Image
+              source={require("../../assets/images/z1-01.png")}
+              style={{
+                width: "100%",
+                objectFit: "contain",
+                height: 30,
+              }}
+            />
+          </View>
+          <View
+            style={{
+              height: "100%",
+              flex: 0.7,
+              justifyContent: "flex-end",
+              display: "flex",
+              flexDirection: "row",
+              // backgroundColor: colors.red,
+              alignContent: "flex-end",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 13,
+                fontWeight: "bold",
+                color: colors.darkGrey,
+              }}
+            >
+              Track
+            </Text>
+
+            <Switch
+              style={{
+                alignSelf: "center",
+              }}
+              value={status === "started"}
+              onValueChange={(value) =>
+                value ? startLocation() : stopLocation()
+              }
+            />
+          </View>
+        </View>
+      </View>
+
       <Button title="Start" onPress={startLocation} />
       <Button title="Stop" onPress={stopLocation} />
       <Text>Status: {status}</Text>
       <StatusBar style="auto" />
+
+      <Button
+        title="Logout"
+        onPress={async () => {
+          stopLocation();
+          await AsyncStorage.removeItem("number");
+          router.push("/screens/auth/login");
+        }}
+      />
     </View>
   );
 }
